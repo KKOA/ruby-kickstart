5882815716508010fe0db6b27e67ee210c597f64
keith(Desktop - mac) <keithamoah@yahoo.co.uk>
2017-08-13 11:51:49 +0100

    completed session2/challenge/2_input_output_control.rb

--- session2/challenge/2_input_output_control.rb
+++ session2/challenge/2_input_output_control.rb
@@ -1,5 +1,5 @@
 # Prompt the user for a number, then read it in and print out "hi" that many times
-# 
+#
 # Repeat this process until the user submits "bye", then say "goodbye" and end the program
 # HINT: Check out example 2 if you get stuck
 
@@ -19,13 +19,23 @@
 # and when you think it is correct, you can test it with  $ rake 2:2
 
 def hi_hi_goodbye
-  # your code here
+  puts "Enter a postive number or bye"
+  user_input = gets.chomp
+  while(user_input != 'bye')
+    if(user_input.to_i <= 0) #user enter invalid characters or number greater than 0
+      puts "You have not entered a postive or bye. Please try again "
+    else
+       puts 'hi ' * user_input.to_i , "Enter a postive number or bye"
+     end
+    user_input = gets.chomp
+  end
+  puts "goodbye"
 end
 
 
 
 
 # This will just invoke the method if you run this program directly
-# This way you can try it out by running "$ ruby 2_input_output_control.rb" 
+# This way you can try it out by running "$ ruby 2_input_output_control.rb"
 # but it will still work for our tests
 hi_hi_goodbye if $0 == __FILE__
