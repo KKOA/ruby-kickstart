7759c7507c824dd11de8e2567459618eead637a7
keith(Desktop - mac) <keithamoah@yahoo.co.uk>
2017-08-13 11:52:34 +0100

    completed session2/challenge/add 3_array.rb

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -7,5 +7,16 @@
 
 class String
   def every_other_char
+  new_string = ""
+    each_char.with_index {|char,pos|
+      #loop through each character in an string assign current character to temporary variable(char) and it's position to pos
+      new_string << char if pos.even?
+    }
+    return new_string
   end
 end
+
+#s = String.new
+p "abcdefg".every_other_char  # => "aceg"
+#p s.every_other_char("abcdefg")
+#p s.every_other_char("")
