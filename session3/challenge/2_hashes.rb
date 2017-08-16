3f45ce6163328cd9caf6754aa3c3b264273c66dd
keith(Desktop - mac) <keithamoah@yahoo.co.uk>
2017-08-16 17:44:36 +0100

    completed session3/challenge/2_hashes.rb

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,14 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase(number)
+  to_return = {}
+  all = (1..number).to_a #create array contain from 1 to argument
+  all.each do | current_num |
+    next if current_num.even?
+    to_return[current_num] = all.select {|i| i.even? && i < current_num } #assign the even number that are less current number
+  end
+  return to_return
+end
+
+puts staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
